e0e3319cde1e080f4571b26568d7741f1c61aced
Viet Anh <vanhhavesomuchcute@gmail.com>
2023-04-06 01:07:19 +0700

    Edit access token.

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const NodeCache = require("node-cache");
 
 const config = {
   pageToken:
-    "EAANOlelNYBsBAODQdt4kLkgmZCUyjs5tZAqYG5yvrbZCpqS8Bf6o9DgAJ7sePXKcqKZCsaZBqMGd8UMRcazjQQ3I3ZCA3Kh7GiyafVDt1HjvmqWNpk13cjU2ZAAbpv5rb8A4FBy6HixaMEDeu7ddD0ZCoATTn6K5X1osiEhPBWcNpauJylJM5JVC", // Đưa token của fanpage của bạn vô đây.
+    "EAANOlelNYBsBAJJwmOR4BcNoObbdEZAAhXZCAIBZBswI6tuVBjZB6e0eeTx56OJcT3Rky9VKrsxSJ4L4Rff8l27TZA4UmJ9UwXXMRlbf9029izTor2MFkC9vf3lRvILoU527TnCYZADdnu3K5mGuu1JhFOGDUNRewLZCMYf3ZBnR3ZCBcG5aNelwZB", // Đưa token của fanpage của bạn vô đây.
   appSecret: "", // Cái này có thể có hoặc không, nếu có thì nó càng bảo mật.
   verifyToken: "ngocisnumber1", // Nhập cái gì vô đây cũng được nhưng nhớ copy vào Verify Token ở mục Webhooks.
   openai: {
     model: "gpt-3.5-turbo", // Model ChatGPT (gpt-3-turbo, gpt-3, gpt-4)
-    token: "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", // API Token của ChatGPT
+    token: "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", // API Token của ChatGPT
     max_tokens: 2048, // Bạn có thể thay đổi theo bạn muốn.
   },
   mode: "CHAT", // bạn có thể chỉnh thành IMAGE để bot tạo ảnh nhé.
